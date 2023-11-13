79b19402a5408e1bd2ca7cbf3f654d1851562203
keranis <mahdihadjsassi@fsb.u-carthage.tn>
2023-11-13 19:30:54 +0100

    deploy app

--- approve-request.php
+++ approve-request.php
@@ -63,7 +63,7 @@ else if($hour>12){$greeting='Good Afternoon';}
                 <body style="font: small/ 1.5 Arial,Helvetica,sans-serif;">
                 <div style="max-width:602px;margin:0 auto;background:#f6f7f9;padding:20px;box-sizing: border-box;"  bgcolor="#f6f7f9">
                     <div style="width:100%;height:30px;display:flex;margin-bottom: 30px;">
-                        <img src="https://ensitgeeksclub.com/joinUs/images/logo-geeks.png" width="30px" />
+                        <img src="https://ensitgeeksclub.com/assets/img/logo-geeks.png" width="30px" />
                         <div style="display:flex;align-items: center;height:100%;margin-left:10px;font-size:20px;font-weight: bold;">ENSIT Geeks Club</div>
                     </div>
                     <div style="width:100%;background:white;box-sizing: border-box;padding:30px;">
--- inscri.php
+++ inscri.php
@@ -1,7 +1,7 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
 
 require 'serverAdd.php';
 require 'mail.php';
@@ -147,7 +147,7 @@ if ($uploadOk == 0) {
                 <body style="font: small/ 1.5 Arial,Helvetica,sans-serif;">
                 <div style="max-width:602px;margin:0 auto;background:#f6f7f9;padding:20px;box-sizing: border-box;"  bgcolor="#f6f7f9">
                     <div style="width:100%;height:30px;display:flex;margin-bottom: 30px;">
-                        <img src="https://ensitgeeksclub.com/joinUs/images/logo-geeks.png" width="30px" />
+                        <img src="https://ensitgeeksclub.com/assets/img/logo-geeks.png" width="30px" />
                         <div style="display:flex;align-items: center;height:100%;margin-left:10px;font-size:20px;font-weight: bold;">ENSIT Geeks Club</div>
                     </div>
                     <div style="width:100%;background:white;box-sizing: border-box;padding:30px;">
@@ -183,7 +183,7 @@ if ($uploadOk == 0) {
                             <body style="font: small/ 1.5 Arial,Helvetica,sans-serif;">
                             <div style="max-width:602px;margin:0 auto;background:#f6f7f9;padding:20px;box-sizing: border-box;"  bgcolor="#f6f7f9">
                                 <div style="width:100%;height:30px;display:flex;margin-bottom: 30px;">
-                                    <img src="https://ensitgeeksclub.com/joinUs/images/logo-geeks.png" width="30px" />
+                                    <img src="https://ensitgeeksclub.com/assets/img/logo-geeks.png" width="30px" />
                                     <div style="display:flex;align-items: center;height:100%;margin-left:10px;font-size:20px;font-weight: bold;">ENSIT Geeks Club</div>
                                 </div>
                                 <div style="width:100%;background:white;box-sizing: border-box;padding:30px;">
@@ -255,7 +255,7 @@ if ($uploadOk == 0) {
                 <body style="font: small/ 1.5 Arial,Helvetica,sans-serif;">
                 <div style="max-width:602px;margin:0 auto;background:#f6f7f9;padding:20px;box-sizing: border-box;"  bgcolor="#f6f7f9">
                     <div style="width:100%;height:30px;display:flex;margin-bottom: 30px;">
-                        <img src="https://ensitgeeksclub.com/joinUs/images/logo-geeks.png" width="30px" />
+                        <img src="https://ensitgeeksclub.com/assets/img/logo-geeks.png" width="30px" />
                         <div style="display:flex;align-items: center;height:100%;margin-left:10px;font-size:20px;font-weight: bold;">ENSIT Geeks Club</div>
                     </div>
                     <div style="width:100%;background:white;box-sizing: border-box;padding:30px;">
@@ -291,7 +291,7 @@ if ($uploadOk == 0) {
                             <body style="font: small/ 1.5 Arial,Helvetica,sans-serif;">
                             <div style="max-width:602px;margin:0 auto;background:#f6f7f9;padding:20px;box-sizing: border-box;"  bgcolor="#f6f7f9">
                                 <div style="width:100%;height:30px;display:flex;margin-bottom: 30px;">
-                                    <img src="https://ensitgeeksclub.com/joinUs/images/logo-geeks.png" width="30px" />
+                                    <img src="https://ensitgeeksclub.com/assets/img/logo-geeks.png" width="30px" />
                                     <div style="display:flex;align-items: center;height:100%;margin-left:10px;font-size:20px;font-weight: bold;">ENSIT Geeks Club</div>
                                 </div>
                                 <div style="width:100%;background:white;box-sizing: border-box;padding:30px;">
