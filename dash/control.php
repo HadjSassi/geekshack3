1c2e5312bf315e5bff2e54da7570359143dd40bd
keranis <mahdihadjsassi@fsb.u-carthage.tn>
2023-11-20 23:47:50 +0100

    update problems

--- dash/control.php
+++ dash/control.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-function authenticate($password) {
+function authenticate($password)
+{
     $credentials = json_decode(file_get_contents('credentials.json'), true);
     $algorithm = "sha256"; // You can use other algorithms like sha256, sha512, etc.
 
@@ -25,11 +26,12 @@ $link = mysqli_connect('localhost', 'root', "geeks_hack_2023", 'geekshack3');
 
 
 //nb des particiapnts 
-$requete2= "select team_tag,username,nom,prenom,email,phone,ecole from team,candidat where team.leader_username = candidat.username;";
+$requete2 = "select team_tag,username,nom,prenom,email,phone,ecole from team,candidat where team.leader_username = candidat.username;";
 $result2 = mysqli_query($link, $requete2);
 $nb_condidat = $result2->num_rows;
 
-function getProblemDetails($problemFolder) {
+function getProblemDetails($problemFolder)
+{
     $problemFiles = glob($problemFolder . '/* *.html');
     $difficulty = "unknown";  // Default difficulty if not determined
     $problemNumber = basename($problemFolder);
@@ -78,7 +80,8 @@ foreach ($problemFolders as $problemFolder) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
     <title>Dashboard GeeksHack</title>
-    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <link rel="stylesheet"
+          href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
     <link rel="stylesheet" href="css/style.css">
     <!--    <link rel="stylesheet" type="text/css" href="../vendor/bootstrap/css/bootstrap.min.css">-->
     <link rel="icon" href="../assets/logo-geeks.png"/>
@@ -92,7 +95,8 @@ foreach ($problemFolders as $problemFolder) {
         <table>
             <tr>
                 <td><img src="geeks.png" style="width: 45px; margin-right: -7px; border-radius:40%; "></img></td>
-                <td><h2 onclick="window.location.href='/'" style="cursor:pointer;margin-top: 7px; margin-left: 2px; color: white;">Geeks Club</h2> </td>
+                <td><h2 onclick="window.location.href='/'"
+                        style="cursor:pointer;margin-top: 7px; margin-left: 2px; color: white;">Geeks Club</h2></td>
             </tr>
 
         </table>
@@ -107,7 +111,8 @@ foreach ($problemFolders as $problemFolder) {
             </li>
             <li>
                 <span class="las la-users"></span>
-                <span style="cursor: pointer" onclick="window.location.href='control_candidates.php'">Control Candidates</span>
+                <span style="cursor: pointer"
+                      onclick="window.location.href='control_candidates.php'">Control Candidates</span>
             </li>
             <li>
                 <span class="las la-users"></span>
@@ -119,10 +124,8 @@ foreach ($problemFolders as $problemFolder) {
             </li>
             <li>
                 <span class="las la-door-open"></span>
-                <span class="" onclick="window.location.href='logout.php'" style="cursor: pointer" >Logout</span>
+                <span class="" onclick="window.location.href='logout.php'" style="cursor: pointer">Logout</span>
             </li>
-
-
 
 
         </ul>
@@ -139,12 +142,12 @@ foreach ($problemFolders as $problemFolder) {
 
         <div class="search-wrapper">
             <span class="las la-search"></span>
-            <input type="search" placeholder="Rechercher" id="myInput" onkeyup='tableSearch()' />
+            <input type="search" placeholder="Rechercher" id="myInput" onkeyup='tableSearch()'/>
         </div>
     </header>
 
     <main>
-        <div class="recent-grid" >
+        <div class="recent-grid">
             <div class="projects">
                 <div class="card">
                     <div class="card-header">
@@ -153,7 +156,8 @@ foreach ($problemFolders as $problemFolder) {
 
                     <div class="card-body">
                         <div class="table-responsive">
-                            <table width="120%"  id ="myTable" data-filter-control="true" data-show-search-clear-button="true">
+                            <table width="120%" id="myTable" data-filter-control="true"
+                                   data-show-search-clear-button="true">
                                 <thead>
                                 <tr>
                                     <td>Problem Number</td>
@@ -163,12 +167,21 @@ foreach ($problemFolders as $problemFolder) {
                                 </tr>
                                 </thead>
                                 <tbody>
-                                <?php foreach ($problemsData as $problem) { ?>
+                                <?php
+                                // Function to compare problem numbers numerically
+                                function compareProblemNumbers($a, $b) {
+                                    $numericA = intval(preg_replace('/[^\d]/', '', $a['problemNumber']));
+                                    $numericB = intval(preg_replace('/[^\d]/', '', $b['problemNumber']));
+                                    return $numericA - $numericB;
+                                }
+
+                                // Sort the problemsData array using the custom comparison function
+                                usort($problemsData, 'compareProblemNumbers');
+
+                                foreach ($problemsData as $problem) {
+                                    $numericProblemNumber = preg_replace('/[^\d]/', '', $problem['problemNumber']);
+                                    ?>
                                     <tr>
-                                        <?php
-                                        // Extract only numeric part from problem number
-                                        $numericProblemNumber = preg_replace('/[^\d]/', '', $problem['problemNumber']);
-                                        ?>
                                         <td onclick="window.location.href='../problems_all/<?php echo $problem['problemNumber']; ?>/<?php echo $problem['problemNumber']; ?> <?php echo $problem['difficulty']; ?>.html'"
                                             style="cursor: pointer;"><?php echo $numericProblemNumber; ?></td>
                                         <td onclick="window.location.href='../problems_all/<?php echo $problem['problemNumber']; ?>/<?php echo $problem['problemNumber']; ?> <?php echo $problem['difficulty']; ?>.html'"
@@ -178,12 +191,19 @@ foreach ($problemFolders as $problemFolder) {
 
                                         <td>
                                             <?php if ($_SESSION['role'] === 'admin') { ?>
-                                                <button class="btn btn-primary" style="margin-right: 3%;" onclick="copyProblemFolder('<?php echo $problem['problemNumber']; ?>')">Add</button>
-                                                <button class="btn btn-danger" style="" onclick="deleteProblemFolder('<?php echo $problem['problemNumber']; ?>')">Revoke</button>
+                                                <button class="btn btn-primary" style="margin-right: 3%;"
+                                                        onclick="copyProblemFolder('<?php echo $problem['problemNumber']; ?>')">
+                                                    Add
+                                                </button>
+                                                <button class="btn btn-danger" style=""
+                                                        onclick="deleteProblemFolder('<?php echo $problem['problemNumber']; ?>')">
+                                                    Revoke
+                                                </button>
                                             <?php } else { ?>
                                                 <!-- Display a message or a disabled button for non-admin users -->
-                                                <button class="btn btn-primary disabled" style="margin-right: 3%;">Add</button>
-                                                <button class="btn btn-danger disabled" style="" >Revoke</button>
+                                                <button class="btn btn-primary disabled" style="margin-right: 3%;">Add
+                                                </button>
+                                                <button class="btn btn-danger disabled" style="">Revoke</button>
                                             <?php } ?>
                                         </td>
                                     </tr>
@@ -208,12 +228,13 @@ foreach ($problemFolders as $problemFolder) {
                             <div class="customer">
                                 <div class="info">
                                     <div>
-                                        <h4>    <?php echo $row['team_tag'].' <> '.$row['username'].' <br> <h5>'.$row['nom'].' '.$row['prenom'].'</h5>' ?> </h4>
-                                        <small> <?php echo $row['ecole'].' <> '.$row['phone'] ?> </small>
+                                        <h4>    <?php echo $row['team_tag'] . ' <> ' . $row['username'] . ' <br> <h5>' . $row['nom'] . ' ' . $row['prenom'] . '</h5>' ?> </h4>
+                                        <small> <?php echo $row['ecole'] . ' <> ' . $row['phone'] ?> </small>
                                     </div>
                                 </div>
                                 <div class="contact">
-                                    <a href="mailto: <?php echo $row['email'] ?> ">  <span class="las la-envelope-open" ></span></a>
+                                    <a href="mailto: <?php echo $row['email'] ?> "> <span
+                                                class="las la-envelope-open"></span></a>
                                 </div>
                             </div>
 
@@ -242,7 +263,7 @@ foreach ($problemFolders as $problemFolder) {
             td1 = tr[i].getElementsByTagName("td")[0];
             td2 = tr[i].getElementsByTagName("td")[1];
             if (td1 || td2) {
-                txtValue =( td1.textContent || td1.innerText)+( td2.textContent || td2.innerText);
+                txtValue = (td1.textContent || td1.innerText) + (td2.textContent || td2.innerText);
                 if (txtValue.toUpperCase().indexOf(filter) > -1) {
                     tr[i].style.display = "";
                 } else {
@@ -254,7 +275,7 @@ foreach ($problemFolders as $problemFolder) {
     }
 
 
-    function pickUp(){
+    function pickUp() {
         score = document.getElementById("score");
     }
 
@@ -274,6 +295,7 @@ foreach ($problemFolders as $problemFolder) {
         };
         xhr.send("problemNumber=" + problemNumber);
     }
+
     function deleteProblemFolder(problemNumber) {
         // Send an AJAX request to the PHP script to copy the folder
         var xhr = new XMLHttpRequest();
