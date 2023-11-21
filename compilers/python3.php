d44c32247b6bc4c3bad366669d516df271af8529
keranis <mahdihadjsassi@fsb.u-carthage.tn>
2023-11-21 20:20:50 +0100

    Execution Time

--- compilers/python3.php
+++ compilers/python3.php
@@ -2,9 +2,7 @@
 session_start();
 $prob_name = $_SESSION['namep'];
 $id = $_SESSION['ID'];
-// Read timeout value from file, default to 3 seconds if not available
-$timeoutFilePath = "problems/".$prob_name."/titre.txt";
-$timeing = (file_exists($timeoutFilePath) && ($lines = file($timeoutFilePath)) && isset($lines[1])) ? intval(trim($lines[1])) : 2;
+$timeing = 1;
 $CC = "timeout ".$timeing."s python3";
 $code = $_POST["code"];
 //$input = $_POST["input"];
